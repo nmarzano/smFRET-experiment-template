f71ae7c64a0ebf1349a88ae70e3172a2d8dc8e71
nmarzano <80363768+nmarzano@users.noreply.github.com>
2021-08-01 16:24:07 +1000

    add ridgeline plot

--- Experiment_1-description/analysis_scripts/1A-plot_histogram.py
+++ Experiment_1-description/analysis_scripts/1A-plot_histogram.py
@@ -66,4 +66,51 @@ plt.legend([labels[treatment] for treatment in reversed(order)],loc='upper left'
 plot_hist.figure.savefig(f'{output_folder}/Histogram.svg', dpi = 600)
 plt.show()
 
+###### FOR RIDGELINE PLOTS
+###################################################
+###################################################
+data_paths_ridgeline = {
+    "1":"label",
+    '2':'label',
+    "3":"label",
+    "4":"label", 
+    "5":"label", 
+    "6":"label",
+    "7":"label", 
+    "8":"label", 
+    "9":"label", 
+    "10":"label", 
+    "11":"label", 
+    "12":"label",
+}
 
+pal = sns.color_palette(palette='mako', n_colors=12)
+g = sns.FacetGrid(compiled_df, row='treatment_name', hue='treatment_name', aspect=10, height=5, palette=pal)
+# then we add the densities kdeplots for each condition
+g.map(sns.kdeplot, 'FRET',
+      bw_adjust=1, clip_on=False,
+      fill=True, alpha=1, linewidth=10)
+# here we add a white line that represents the contour of each kdeplot
+g.map(sns.kdeplot, 'FRET', 
+      bw_adjust=1, clip_on=False, 
+      color="white", linewidth=6)
+# here we add a horizontal line for each plot
+g.map(plt.axhline, y=0,
+      lw=2, clip_on=False)
+# we loop over the FacetGrid figure axes (g.axes.flat) and add the condition as text with the right color
+# notice how ax.lines[-1].get_color() enables you to access the last line's color in each matplotlib.Axes
+for i, ax in enumerate(g.axes.flat):
+    ax.text(0, .5, data_paths_ridgeline[f'{i+1}'],
+            fontweight='bold', fontsize=100,
+            color=ax.lines[-1].get_color())
+    ax.set_facecolor((0, 0, 0, 0))  ### removes background so KDE plots are not obstructed when stacked
+# we use matplotlib.Figure.subplots_adjust() function to get the subplots to overlap
+g.fig.subplots_adjust(hspace=-0.7)
+# eventually we remove axes titles, yticks and spines
+g.set_titles("")
+g.set(yticks=([]))
+g.despine(bottom=True, left=True)
+plt.setp(ax.get_xticklabels(), fontsize=100, fontweight='bold')
+plt.xlabel('FRET', fontweight='bold', fontsize=100)
+g.savefig(f'{output_folder}/Histogram-ridgeline.svg', dpi = 600)
+plt.show()
