f47653fd5e6e946ce9d483f7f80b5a58cf2a781d
nmarzano <80363768+nmarzano@users.noreply.github.com>
2021-11-15 18:02:36 +1100

    update TDP cleanup for better analysis downstream

--- Experiment_1-description/analysis_scripts/2A-initial_cleanup_TDP.py
+++ Experiment_1-description/analysis_scripts/2A-initial_cleanup_TDP.py
@@ -22,12 +22,14 @@ def remove_outliers(compiled_TDP):
 def calculate_dwell_time(df):
     df_test2 = []
     for Molecule, dfs in df.groupby('Molecule'):
+        frame_length = len(dfs)
         test = dfs.groupby([dfs['Idealized_FRET'].ne(dfs['Idealized_FRET'].shift()).cumsum(), 'Idealized_FRET']).size()
         test = test.reset_index(level = 1, drop = False)
         test['Molecule'] = Molecule
+        test['number_of_frames'] = frame_length
         df_test2.append(test)
     df_test3 = pd.concat(df_test2)
-    df_test3.columns = ['FRET_state', 'Time', 'Molecule']
+    df_test3.columns = ['FRET_state', 'Time', 'Molecule', 'number_of_frames']
     df_test3 = df_test3.reset_index().drop('Idealized_FRET', axis = 1)
     df_test4 = df_test3[df_test3.groupby('Molecule').Molecule.transform('count') > 1]
     return df_test4
@@ -38,8 +40,8 @@ def generate_transitions(df):
         poop = dfs.assign(FRET_after = dfs.FRET_state.shift(-1)).dropna()
         df_toconcat.append(poop)
     compiled_df = pd.concat(df_toconcat).reset_index(drop = True)
-    compiled_final = compiled_df[['Molecule', 'FRET_state', 'FRET_after', 'Time']]
-    compiled_final.columns = ['Molecule', 'FRET_before', 'FRET_after', 'Time']
+    compiled_final = compiled_df[['Molecule', 'FRET_state', 'FRET_after', 'Time', 'number_of_frames']]
+    compiled_final.columns = ['Molecule', 'FRET_before', 'FRET_after', 'Time', 'number_of_frames']
     return compiled_final
 
 from Utilities.Data_analysis import filter_TDP, file_reader, count_filt_mol
@@ -54,6 +56,17 @@ for data_name, data_path in data_paths.items():
     compiled_TDP.append(cleaned_TDPdata)
 compiled_TDP = pd.concat(compiled_TDP)   
 compiled_TDP.to_csv(f'{output_folder}/TDP_cleaned.csv', index = False)
+
+raw_TDPdata = []
+for data_name, data_path in data_paths.items():
+    imported_TDPdata = file_reader(data_path, 'TDP')
+    df1 = calculate_dwell_time(imported_TDPdata)
+    df2 = generate_transitions(df1)
+    # cleaned_TDPdata = remove_outliers(df2)
+    df2["treatment_name"] = data_name
+    raw_TDPdata.append(df2)
+compiled_raw_TDP = pd.concat(raw_TDPdata)   
+compiled_raw_TDP.to_csv(f'{output_folder}/TDP_raw.csv', index = False)
 
 filtered_data = filter_TDP(compiled_TDP, 0.3)  ##### number refers to the FRET threshold to filter data. Will only include molecules that go below this set threshold. Will default to 0.3 
 filtered_data.to_csv(f'{output_folder}/TDP_cleaned_filt.csv', index = False)
