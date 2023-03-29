563108e5df96bb793861606a581fdc25375fd4bc
nmarzano <80363768+nmarzano@users.noreply.github.com>
2023-03-29 16:15:32 +1100

    add func to identify consecutive increases in FRET

--- Experiment_1-description/analysis_scripts/1F-synchronised-transition.py
+++ Experiment_1-description/analysis_scripts/1F-synchronised-transition.py
@@ -8,10 +8,15 @@ import glob
 import os
 from Utilities.Data_analysis import filter_TDP, file_reader, count_filt_mol
 
-output_folder = 'Experiment/python_results'
+output_folder = 'Experiment_1-description/python_results'
+plot_export = f'{output_folder}/synchronised_transitions/'
+if not os.path.exists(plot_export):
+    os.makedirs(plot_export)
 compiled_data = pd.read_csv(f'{output_folder}/Cleaned_FRET_histogram_data.csv')
-FRET_value = 0.5 #### the proportion of molecules that travel below this threshold will be counted
+FRET_value = 0.2 #### the proportion of molecules that travel below this threshold will be counted
 exposure = 0.2 # in seconds
+order = ['1', '2', '3', '4']
+list_to_drop = ['']
 
 ###########
 ########### Calculates the dwell time for each idealized FRET state based on the HMM fits and then appends the dwell duration to the cleaned FRET histogram data.
@@ -79,16 +84,6 @@ def generate_transitions(dfs):
     return pd.concat(compiled_transition)
 
 
-calculated_transitions = []
-for treatment, df in compiled_data.groupby('treatment_name'):
-    dwell_df = calculate_dwells(df)
-    transition_df = generate_transitions(dwell_df)
-    calculated_transitions.append(transition_df)
-calculated_transitions_df = pd.concat(calculated_transitions)
-calculated_transitions_df.reset_index(inplace = True)
-calculated_transitions_df.drop('index', axis = 1, inplace = True)
-
-
 
 #########
 ######### Filters the dataset prior to plotting. Will filter based on the type of transition (low-to-high or vice-versa) and for how long the dwell state exists prior to a transition.
@@ -116,8 +111,7 @@ def filt_df_to_plot(df, FRET_before, FRET_after, transition_type = 'low_to_high'
     return index_to_plot
 
 
-
-def plot_synchronised_transition(dfs, index_to_plot, exposure_seconds, list_to_drop, frame_from_trans = 80):
+def plot_synchronised_transition(dfs, index_to_plot, exposure_seconds, list_to_drop, order, frame_from_trans = 80, label = ''):
     """plots the FRET values either side of a transition type of interest
 
     Args:
@@ -139,25 +133,111 @@ def plot_synchronised_transition(dfs, index_to_plot, exposure_seconds, list_to_d
     filt_data = combined_mini[~combined_mini['treatment_name'].isin(list_to_drop)]
     fig, axes = plt.subplots()
     sns.set(style = 'ticks')
-    sns.lineplot(data = filt_data, x = 'time_from_trans', y = 'FRET', hue = 'treatment_name', palette = 'BuPu')
+    sns.lineplot(data = filt_data, x = 'time_from_trans', y = 'FRET', hue = 'treatment_name', palette = 'BuPu', hue_order = order)
     plt.xlabel('Time (s)')
-    plt.legend(title = '', loc = 'best')
+    plt.legend(title = '',loc = 'best')
+    fig.savefig(f'{plot_export}/synchronised_release{"_"+label}.svg', dpi = 600)
     plt.show()
 
 
-list_to_drop = ['']
-dnak_stable_release = filt_df_to_plot(calculated_transitions_df, 0.5, 0.5,'low_to_high', 10)
-plot_synchronised_transition(calculated_transitions_df, dnak_stable_release, exposure, list_to_drop, 10)
+def determine_first_transition_in_sequence(dfs):
+    combined_data = []
+    for molecule, df in dfs.groupby('molecule_number'):
+        trans_list = df['FRET_increase'].to_list()
+        df['output_column'] = [
+        1 if all([(x == 0), (trans_list[x] == True), (trans_list[x+1] == True)]) else 
+        (1 if all([(trans_list[x-1] == False),(trans_list[x] == True), (trans_list[x+1] == True)]) else 0)
+        for x in range(len(trans_list)-1)]+[0]
+        combined_data.append(df)
+    return pd.concat(combined_data)
 
-dnak_stable_binding = filt_df_to_plot(calculated_transitions_df, 0.5, 0.5, 'high_to_low', 10)
-plot_synchronised_transition(calculated_transitions_df, dnak_stable_binding, exposure, list_to_drop, 10)
+def concat_trans_proportion(dfs, raw_df, FRET_before, FRET_after):
+    #### create dataframes filtered to have only consecutive or non-consecutive transitions
+    consecutive_trans = dfs[dfs['output_column']==True]
+    nonconsecutive_trans = dfs[dfs['output_column']==False]
+
+    ##### determine the total number of transitions and the number of transitions that meet a criteria
+    transitions_only = raw_df[raw_df['transition_point'] == True]
+    total_trans = transitions_only.groupby('treatment_name')['transition_point'].sum()
+    transitions_above_thresh = transitions_only[(transitions_only['FRET_before'] < FRET_before) & (transitions_only['FRET_after'] > FRET_after)]
+    trans_above_thresh = transitions_above_thresh.groupby('treatment_name')['transition_point'].sum()
+    percent_trans_meet_criteria = (trans_above_thresh/total_trans)*100
+    percent_trans_meet_criteria_df = pd.DataFrame(percent_trans_meet_criteria).reset_index()
+    percent_trans_meet_criteria_df.columns = ['treatment', '% trans DnaK release']
+
+    #### determine the total number of consecutive transitions that meet a criteria
+    number_consecutive_event = consecutive_trans.groupby('treatment_name')['output_column'].sum()
+    consecutive_event_above_thresh = consecutive_trans[(consecutive_trans['FRET_before'] < FRET_before) & (consecutive_trans['FRET_after'] > FRET_after)]
+    number_consecutive_event_meet_criteria = consecutive_event_above_thresh.groupby('treatment_name')['output_column'].sum()
+    percent_of_consecutive_event_from_DnaK = (number_consecutive_event_meet_criteria/number_consecutive_event)*100
+    percent_of_consecutive_event_from_DnaK = pd.DataFrame(percent_of_consecutive_event_from_DnaK).reset_index()
+
+    percent_of_DnaK_release_events_that_are_consecutive = pd.DataFrame((number_consecutive_event_meet_criteria/trans_above_thresh)*100).reset_index()
+    percent_of_DnaK_release_events_that_are_consecutive.columns = ['treatment', 'proportion_consecutive_from_DnaK']
+
+    ### merge columns
+    percent_trans_meet_criteria_df['% DnaK release are consecutive'] = percent_of_DnaK_release_events_that_are_consecutive['proportion_consecutive_from_DnaK']
+    percent_trans_meet_criteria_df['% consecutive events are DnaK release'] = percent_of_consecutive_event_from_DnaK['output_column']
+    return consecutive_trans, nonconsecutive_trans, percent_trans_meet_criteria_df
 
 
+calculated_transitions = []
+for treatment, df in compiled_data.groupby('treatment_name'):
+    dwell_df = calculate_dwells(df)
+    transition_df = generate_transitions(dwell_df)
+    calculated_transitions.append(transition_df)
+calculated_transitions_df = pd.concat(calculated_transitions)
+calculated_transitions_df.reset_index(inplace = True)
+calculated_transitions_df.drop('index', axis = 1, inplace = True)
 
-list_to_drop = ['PPR_alone', 'RNA05']
-dnak_stable_release = filt_df_to_plot(calculated_transitions_df, 0.5, 0.5,'low_to_high', 30)
-plot_synchronised_transition(calculated_transitions_df, dnak_stable_release, exposure, list_to_drop, 30)
+font = {'weight' : 'normal', 'size'   : 12 }
+plt.rcParams['font.sans-serif'] = "Arial"
+plt.rcParams['font.family'] = "sans-serif"
+plt.rc('font', **font)
+plt.rcParams['svg.fonttype'] = 'none'
 
-dnak_stable_binding = filt_df_to_plot(calculated_transitions_df, 0.5, 0.5, 'high_to_low', 0)
-plot_synchronised_transition(calculated_transitions_df, dnak_stable_binding, exposure, list_to_drop, 100)
+dnak_stable_release = filt_df_to_plot(calculated_transitions_df, 0.2, 0.2,'low_to_high', 200)
+plot_synchronised_transition(calculated_transitions_df, dnak_stable_release, exposure, list_to_drop, order, 200, 'release')
+
+dnak_stable_binding = filt_df_to_plot(calculated_transitions_df, 0.2, 0.2, 'high_to_low', 50)
+plot_synchronised_transition(calculated_transitions_df, dnak_stable_binding, exposure, list_to_drop, order, 50, 'binding')
+
+
+############
+############ Code past this point is to investigate the difference between consecutive and non-consecutive transitions
+############
+col = []
+for treatment, df in calculated_transitions_df.groupby('treatment_name'):
+    transition_data = df[df['transition_point']==True]
+    transition_data['FRET_increase'] = transition_data['FRET_before'] < transition_data['FRET_after'] 
+    consecutive_identified = determine_first_transition_in_sequence(transition_data)
+    col.append(consecutive_identified)
+consecutive_data = pd.concat(col)
+
+consecutive_trans, nonconsecutive_trans, percent_trans_meet_criteria_df = concat_trans_proportion(
+    consecutive_data, 
+    calculated_transitions_df, 
+    0.2, 
+    0.2)
+
+####### calling functions to plot synchronised transition
+consecutive_from_dnak_release = filt_df_to_plot(consecutive_trans, 0.2, 0.2,'low_to_high', 200)
+nonconsecutive_from_dnak_release = filt_df_to_plot(nonconsecutive_trans, 0.2, 0.2,'low_to_high', 200)
+
+plot_synchronised_transition(calculated_transitions_df, consecutive_from_dnak_release, exposure, list_to_drop, order, 200, 'consecutive_transitions')
+plot_synchronised_transition(calculated_transitions_df, nonconsecutive_from_dnak_release, exposure, list_to_drop, order, 200, 'non-consecutive_transition')
+
+
+########### Code to plot the percentage transition data 
+
+melted_data = percent_trans_meet_criteria_df.melt(id_vars = 'treatment')
+fig, ax = plt.subplots()
+sns.set_style('ticks',{'grid.linestyle':'--', 'font_scale': 1.5})
+sns.barplot(data = melted_data, y = 'value', x = 'variable', hue = 'treatment', palette = 'BuPu', hue_order = order)
+plt.xlabel('')
+plt.xticks(rotation = 45)
+plt.legend(title = '')
+plt.ylabel('Proportion of transitions (%)')
+fig.savefig(f'{plot_export}/consecutive_transition_summary.svg', dpi = 600)
+plt.show()
 
