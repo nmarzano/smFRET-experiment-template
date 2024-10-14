6c5bfab0702ce421b9d27b1708a4c1c590828d1f
nmarzano <80363768+nmarzano@users.noreply.github.com>
2024-10-14 15:23:36 +1100

    add script to import data from csv

--- Experiment_1/src/two-colour-FRET/0-import_data.py
+++ Experiment_1/src/two-colour-FRET/0-import_data.py
@@ -1,6 +1,11 @@
 from smfret.src.processing_scripts import data_import_script as ps
-
+import pandas as pd
 rawdata_folder = "Experiment_1-description/raw_data"  ## Change for each experiment
 input_folders = ps.locate_raw_drive_files(f'{rawdata_folder}/raw_data.txt')
 ps.move_folders(input_folders, '.dat', rawdata_folder)
 
+# -------- Data from all data sets in the dict will be imported and concatenated into a single dataframe. Outliers wil be removed -----------
+
+output_folder = 'Experiment_2_Flucmutants/python_results'
+data = pd.read_csv('data_directory.csv')
+compiled_df = ps.combine_technical_repeats(data, output_folder)
